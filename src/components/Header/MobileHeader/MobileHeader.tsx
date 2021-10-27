fbe86a183c4a273f8fde7349156b9e5f98d49f18
Vadim Tatarincev <it-vadim@inbox.ru>
2021-10-28 00:03:40 +0300

    Инициировал мобильное меню

--- src/components/Header/MobileHeader/MobileHeader.tsx
+++ src/components/Header/MobileHeader/MobileHeader.tsx
@@ -1,8 +1,36 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { Fab } from 'ui-neumorphism';
+import { useWindowSize } from "../../../hooks";
+import { ThemeContext } from '../../../storage'
+
 import "./MobileHeader.scss";
 
-export const MobileHeader = (): JSX.Element => {
-  const isVisibleMenu = useState<boolean>(false);
+const MobileMenu = (): JSX.Element => {
+  const { isDark } = useContext(ThemeContext)
 
-  return <div className="MobileHeader">MobileHeader</div>;
+
+  return <div className="MobileMenu" style={{
+    backgroundColor: isDark ? '--dark-bg' : '--light-bg',
+  }}>
+    <div className="MobileMenu__Content">
+      Я мобильное меню
+    </div>
+  </div>
+}
+
+export const MobileHeader = (): JSX.Element => {
+  const { isDark } = useContext(ThemeContext)
+  const [isVisibleMenu, setVisibilityMenu] = useState<boolean>(false);
+  const ws = useWindowSize()
+
+  if (ws.device !== 'mobile') {
+    setVisibilityMenu(false)
+  }
+
+  return <div className="MobileHeader">
+    <Fab dark={isDark} size="large" onClick={() => { setVisibilityMenu(!isVisibleMenu) }}>
+      <span style={{ fontSize: '30px', margin: '0 5px 5px' }}>&#43;</span>
+    </Fab>
+    {isVisibleMenu && <MobileMenu />}
+  </div>;
 };
